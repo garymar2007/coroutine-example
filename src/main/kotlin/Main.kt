c1991c6f266219d1d31bdfca45857bef37b7e669
garyma <garymar2007@gmail.com>
2024-08-10 10:55:13 +0200

    Using withContext and join.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,6 +5,29 @@ import kotlinx.coroutines.*
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    // Using withContext to switch the context of the coroutine
+    runBlocking {
+        val bricksJob = launch(Dispatchers.Default) {
+            perform("laying bricks")
+        }
+
+        launch(Dispatchers.IO) {
+            val windows = order(Product.WINDOWS)
+            bricksJob.join()
+            withContext(Dispatchers.Default) {
+                perform("installing ${windows.description}")
+            }
+        }
+
+        launch(Dispatchers.IO) {
+            val doors = order(Product.DOORS)
+            bricksJob.join()
+            withContext(Dispatchers.Default) {
+                perform("installing ${doors.description}")
+            }
+        }
+    }
+
     //Parallel execution of tasks
     // Dispatchers.IO is used to perform blocking IO operations of CPU-heavy tasks(scale
     // up the number of threads to 64 threads by default)
@@ -12,15 +35,15 @@ fun main() {
     //
     // Dispatchers.Default is used to perform IO-heavy tasks (thread pool containing
     // the number of threads equal to the number of CPU cores)
-    runBlocking {
-        val windows = async(Dispatchers.IO) { order(Product.WINDOWS) }
-        val doors = async(Dispatchers.IO) { order(Product.DOORS) }
-        launch(Dispatchers.Default) {
-            perform("laying bricks")
-            launch { perform("installing ${windows.await().description}") }
-            launch { perform("installing ${doors.await().description}") }
-        }
-    }
+//    runBlocking {
+//        val windows = async(Dispatchers.IO) { order(Product.WINDOWS) }
+//        val doors = async(Dispatchers.IO) { order(Product.DOORS) }
+//        launch(Dispatchers.Default) {
+//            perform("laying bricks")
+//            launch { perform("installing ${windows.await().description}") }
+//            launch { perform("installing ${doors.await().description}") }
+//        }
+//    }
 
     //concurrent execution of tasks
 //    runBlocking {
